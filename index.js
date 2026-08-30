17db10522d655c0c462db3b7d9a9940c24c90c19
tyleruploads <tyleruploads@yahoo.com>
2026-08-29 20:02:40 -0400

    Add auto focusing on input on page load and Ctrl + /, and automatic searching on Enter key

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const inputSearch = document.getElementById('input-search');
 const buttonSearch = document.getElementById('button-search');
 
-buttonSearch.addEventListener('click', () => {
+function search() {
     const search = encodeURIComponent(inputSearch.value);
     
     const url = `https://www.google.com/search?q=${search}`;
 
     window.location.href = url;
+ 
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    inputSearch.focus(); // Automatically focus onto input on page load
 });
+
+window.addEventListener('keydown', e => {
+    // Refocus onto input box on Ctrl + /
+    if (e.ctrlKey && e.key === '/') {
+        // Prevent default behavior
+        e.preventDefault();
+
+        inputSearch.focus();
+    }
+});
+
+inputSearch.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        search();
+    }
+});
+
+buttonSearch.addEventListener('click', search);
